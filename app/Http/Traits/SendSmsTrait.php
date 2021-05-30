a380cc3ca90357b2ad992a2e13328e03b5753f1e
Arifin N <arifinofficial@outlook.com>
2021-05-30 20:55:58 +0800

    fix: max timeout

--- app/Http/Traits/SendSmsTrait.php
+++ app/Http/Traits/SendSmsTrait.php
@@ -22,6 +22,8 @@ trait SendSmsTrait
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
         $response = curl_exec($ch);
 
